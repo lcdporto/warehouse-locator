34fed02f0f46847dcb6c99cd0adad693df501323
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2022-05-25 00:07:12 +0100

    Add support for multiple LED strips

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,28 +1,39 @@
 #include <FastLED.h>
 
-#define LED_PIN 33
-#define NUM_LEDS 300
+#define LED_STRIP_1_PIN 33
+#define LED_STRIP_2_PIN 23
+#define LED_STRIP_3_PIN 19
+#define LED_STRIP_4_PIN 22
+#define LED_STRIP_5_PIN 21
+#define LED_STRIP_6_PIN 25
 
-CRGB leds[NUM_LEDS];
+#define NUM_LEDS 144 * 5
 
-void setup()
-{
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+CRGB led_strip_1[NUM_LEDS];
+CRGB led_strip_2[NUM_LEDS];
+CRGB led_strip_3[NUM_LEDS];
+CRGB led_strip_4[NUM_LEDS];
+CRGB led_strip_5[NUM_LEDS];
+CRGB led_strip_6[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812, LED_STRIP_1_PIN, GRB>(led_strip_1, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_2_PIN, GRB>(led_strip_2, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_3_PIN, GRB>(led_strip_3, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_4_PIN, GRB>(led_strip_4, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_5_PIN, GRB>(led_strip_5, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_6_PIN, GRB>(led_strip_6, NUM_LEDS);
 }
 
-void loop()
-{
-  for (uint8_t i = 0; i < 300; i += 19)
-  {
-    for (uint8_t j = 0; j < 19; j++)
-    {
-      leds[i + j] = CRGB(255, 255, 255);
+void loop() {
+  for (uint8_t i = 0; i < 300; i += 19) {
+    for (uint8_t j = 0; j < 19; j++) {
+      led_strip_1[i + j] = CRGB(255, 255, 255);
     }
     FastLED.show();
     delay(1000);
-    for (uint8_t j = 0; j < 19; j++)
-    {
-      leds[i + j] = CRGB(0, 0, 0);
+    for (uint8_t j = 0; j < 19; j++) {
+      led_strip_1[i + j] = CRGB(0, 0, 0);
     }
   }
 }
