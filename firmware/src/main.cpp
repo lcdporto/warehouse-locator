7d63e47e344c84dd254deb69995370c549ee9867
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2022-06-23 00:03:07 +0100

    Base functionality implemented

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,6 +2,7 @@
 #include <FastLED.h>
 #include <PubSubClient.h>
 #include <SPIFFS.h>
+#include <WiFi.h>
 #include <WiFiManager.h>
 
 #define LED_STRIP_1_PIN 33
@@ -12,18 +13,16 @@
 #define LED_STRIP_6_PIN 25
 
 #define NUM_LEDS 144 * 5
+#define NUM_STRIPS 6
 
 #define RESET_BTN_PIN 39
 #define RESET_HOLD_DURATION 5 // seconds
 
 #define MQTT_TOPIC_PREFIX "warehouse-locator/"
 
-CRGB led_strip_1[NUM_LEDS];
-CRGB led_strip_2[NUM_LEDS];
-CRGB led_strip_3[NUM_LEDS];
-CRGB led_strip_4[NUM_LEDS];
-CRGB led_strip_5[NUM_LEDS];
-CRGB led_strip_6[NUM_LEDS];
+char device_id[11] = "";
+
+CRGB led_strips[NUM_STRIPS][NUM_LEDS];
 
 WiFiManagerParameter custom_mqtt_server("mqttserver", "MQTT server address",
                                         "server", 64);
@@ -32,15 +31,21 @@ WiFiManagerParameter custom_mqtt_port("mqttport", "MQTT server port", "1883",
 WiFiManagerParameter custom_mqtt_user("mqttuser", "MQTT username", "user", 16);
 WiFiManagerParameter custom_mqtt_pass("mqttpass", "MQTT password", "pass", 16);
 
-char device_id[64];
-
 WiFiManager wm;
 WiFiClient wifi_client;
 PubSubClient mqtt(wifi_client);
 
 void mqtt_receive(char *topic, byte *payload, unsigned int length) {
-  log_d("Message arrived [%s]", topic);
-  led_strip_1[1] = CRGB(255, 255, 255);
+  log_d("Message arrived [%s]: %s", topic, strndup((char *)payload, length));
+
+  char *topic_prefix = strtok(topic, "/");
+  char *device_id = strtok(NULL, "/");
+  uint8_t strip_n = atoi(strtok(NULL, "/"));
+  uint8_t led_n = atoi(strtok(NULL, "/"));
+
+  led_strips[strip_n][led_n] = strncmp((char *)payload, "on", length) == 0
+                                   ? CRGB(255, 255, 255)
+                                   : CRGB(0, 0, 0);
   FastLED.show();
 }
 
@@ -54,8 +59,8 @@ bool mqtt_connect() {
   if (status == false) {
     return false;
   }
-  char topic[32];
-  sprintf(topic, "%s%s/", MQTT_TOPIC_PREFIX, device_id);
+  char topic[64];
+  sprintf(topic, "%s%s/+/+", MQTT_TOPIC_PREFIX, device_id);
   mqtt.subscribe(topic);
 
   return mqtt.connected();
@@ -88,32 +93,39 @@ void saveParamsCallback() {
 }
 
 void setup() {
-  FastLED.addLeds<WS2812, LED_STRIP_1_PIN, GRB>(led_strip_1, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_STRIP_2_PIN, GRB>(led_strip_2, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_STRIP_3_PIN, GRB>(led_strip_3, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_STRIP_4_PIN, GRB>(led_strip_4, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_STRIP_5_PIN, GRB>(led_strip_5, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_STRIP_6_PIN, GRB>(led_strip_6, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_1_PIN, GRB>(led_strips[0], NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_2_PIN, GRB>(led_strips[1], NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_3_PIN, GRB>(led_strips[2], NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_4_PIN, GRB>(led_strips[3], NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_5_PIN, GRB>(led_strips[4], NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_STRIP_6_PIN, GRB>(led_strips[5], NUM_LEDS);
 
   SPIFFS.begin(true);
   WiFi.mode(WIFI_STA);
 
   if (SPIFFS.exists("/device_id.txt")) {
     fs::File device_id_file = SPIFFS.open("/device_id.txt", "r");
-
-    strcpy(device_id, device_id_file.readString().c_str());
-
+    device_id_file.readBytes(device_id, 11);
     device_id_file.close();
-
   } else {
     fs::File device_id_file = SPIFFS.open("/device_id.txt", "w");
-
-    srand(time(0));
-    sprintf(device_id, "warehouse-locator-%d", rand());
+    sprintf(device_id, "%u", esp_random());
     device_id_file.print(device_id);
-
     device_id_file.close();
   }
+  log_d("Device ID: %s", device_id);
+
+  wm.addParameter(&custom_mqtt_server);
+  wm.addParameter(&custom_mqtt_port);
+  wm.addParameter(&custom_mqtt_user);
+  wm.addParameter(&custom_mqtt_pass);
+  wm.setSaveParamsCallback(saveParamsCallback);
+  wm.setTitle("Warehouse Locator");
+
+  char portal_ssid[32] = "";
+  sprintf(portal_ssid, "warehouse-locator-%u", device_id);
+
+  wm.autoConnect(portal_ssid, "lcdporto");
 
   if (SPIFFS.exists("/config.json")) {
     fs::File configFile = SPIFFS.open("/config.json", "r");
@@ -141,34 +153,22 @@ void setup() {
     }
 
     configFile.close();
+  } else {
+    wm.startConfigPortal(portal_ssid, "lcdporto");
   }
-
-  wm.addParameter(&custom_mqtt_server);
-  wm.addParameter(&custom_mqtt_port);
-  wm.addParameter(&custom_mqtt_user);
-  wm.addParameter(&custom_mqtt_pass);
-  wm.setSaveParamsCallback(saveParamsCallback);
-  wm.setTitle("Warehouse Locator");
-
-  wm.autoConnect(device_id, "lcdporto");
 
   mqtt.setServer(custom_mqtt_server.getValue(),
                  atoi(custom_mqtt_port.getValue()));
-  // mqtt.setCallback(mqtt_receive);
-}
 
-void loop() {
-  for (uint8_t i = 0; i < 7 * 20; i += 19) {
-    for (uint8_t j = 0; j < 19; j++) {
-      led_strip_1[i + j] = CRGB(255, 255, 255);
-    }
-    FastLED.show();
-    delay(1000);
-    for (uint8_t j = 0; j < 19; j++) {
-      led_strip_1[i + j] = CRGB(0, 0, 0);
-    }
+  mqtt.setCallback(mqtt_receive);
+  while (mqtt_connect() == false) {
+    delay(500);
+    log_d("Failed to connect to MQTT. Retrying...");
   }
 
-  mqtt_connect();
-  mqtt.loop();
+  char topic[64];
+  sprintf(topic, "%s%s", MQTT_TOPIC_PREFIX, device_id);
+  mqtt.publish(topic, "online");
 }
+
+void loop() { mqtt.loop(); }
